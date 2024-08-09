208c35a6a2ac98a08128112be812f7d78a41f3b8
Yuldoshova <ozoda8171@gmail.com>
2024-08-09 10:29:20 +0500

    done lesson

--- lesson_3/lesson/backend/server.js
+++ lesson_3/lesson/backend/server.js
@@ -1,11 +1,24 @@
 import http from "node:http"
 import url from 'node:url'
 import fn from './utils/fs.js'
-import { readFile } from "node:fs";
+import { readFile, writeFile } from "node:fs";
+import EventEmitter from "node:events";
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer()
+
+server.on('request', async (req, res) => {
+
+    const errorEvents = new EventEmitter()
+    errorEvents.on('error', (status, message) => {
+        res.writeHead(status, { 'Content-type': 'application/json' })
+        res.end(JSON.stringify({
+            message
+        }))
+        return;
+    })
+
     const method = req.method;
     const path = url.parse(req.url, true).path
     const urls = path.split('/')
@@ -36,8 +49,9 @@ const server = http.createServer((req, res) => {
                 const userId = parseInt(urls[2], 10)
                 const foundUser = allUsers.find((user) => user.id == userId)
                 if (!foundUser) {
-                    res.writeHead(404, { 'Content-type': 'text/html' })
-                    res.end(`<h1 style="color:red">${userId}-user not found!</h1>`)
+                    errorEvents.emit('error', 404, `${userId}-user not found!`)
+                    // res.writeHead(404, { 'Content-type': 'text/html' })
+                    // res.end(`<h1 style="color:red">${userId}-user not found!</h1>`)
                     return;
                 }
                 res.writeHead(200, { 'Content-type': "application/json" })
@@ -45,7 +59,7 @@ const server = http.createServer((req, res) => {
                 return;
             }
 
-         readFile('./data/users.json', 'utf-8', (err, data) => {
+            readFile('./data/users.json', 'utf-8', (err, data) => {
                 if (err) {
                     res.writeHead(500, { 'Content-type': 'text/html' })
                     res.end(`<h1 style="color:red">File o'qishda xatolik!</h1>`)
@@ -58,14 +72,37 @@ const server = http.createServer((req, res) => {
                 }))
                 return;
             })
-   
+
             // res.writeHead(200, { 'Content-type': "application/json" })
             // res.end(JSON.stringify(allUsers, null, 4))
             return;
         }
     }
 
+    if (method === "POST") {
+        if (urls[1] == 'tasks') {
 
+            let newTask = ''
+            req.on('data', (chunk) => {
+                newTask += chunk.toString();
+            })
+
+            req.on('end', () => {
+                const task = JSON.parse(newTask)
+                task.id = allTasks.length + 1;
+                allTasks.push(task);
+                writeFile('./data/tasks.json', JSON.stringify(allTasks, null, 4), (err) => {
+                    if (err) {
+                        errorEvents.emit('error', 500, 'Filega yozishda xatolik!!!');
+                        return;
+                    }
+                })
+            })
+
+            res.end('ok');
+            return;
+        }
+    }
 
     res.writeHead(200, { "Content-type": 'text/html' })
     res.end('<h1 style="color:red">Xato so\'rov yuborildi!</h1>')
